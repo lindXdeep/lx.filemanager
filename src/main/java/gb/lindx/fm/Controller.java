ef66c4ee64a176642ba23baaea2e7f53c554fd72
lindx <linder.igor@gmail.com>
2021-09-17 00:49:43 +0300

    Exit

---
+++ src/main/java/gb/lindx/fm/Controller.java
@@ -0,0 +1,11 @@
+package gb.lindx.fm;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+
+public class Controller {
+  
+  public void btnExitAction(ActionEvent event){
+      Platform.exit();
+  }
+}
