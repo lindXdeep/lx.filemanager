94fb97e204e47e0383e5b144965a4b15a1fd2535
lindx <linder.igor@gmail.com>
2021-09-17 02:52:39 +0300

    read Directory

--- src/main/java/gb/lindx/fm/Controller.java
+++ src/main/java/gb/lindx/fm/Controller.java
@@ -1,15 +1,23 @@
 package gb.lindx.fm;
 
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 
 public class Controller implements Initializable {
 
@@ -31,8 +39,27 @@ public class Controller implements Initializable {
     fileTypeColumn.setCellValueFactory(param -> new SimpleStringProperty(param.getValue().getFileName()));
     fileTypeColumn.setPrefWidth(240);
 
-
     filesTable.getColumns().addAll(fileTypeColumn, fileNameColumn);
 
+    updateList(Paths.get(".", "A"));
+  }
+
+  public void updateList(Path path) {
+   
+    try { // Files.list - может выбросить IOException
+ 
+      filesTable.getItems().clear(); // очищаем элементы в таблице
+      filesTable.getItems().addAll(
+  
+          Files.list(path) // читаем директорию в в иде потока
+              .map(FileInfo::new) // преобразуем в fileInfo
+              .collect(Collectors.toList()) // затем преобразуем в list
+      );
+      filesTable.sort(); // Таблицу сортируем по умоляанию
+
+    } catch (IOException e) {
+      Alert alert = new Alert(AlertType.WARNING, "Can't read files", ButtonType.OK);
+      alert.show();
+    }
   }
 }
